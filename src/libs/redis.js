94857632352f7375a00de751f4af2e5352361dee
snps-madduk <madduk@synopsys.com>
2025-03-12 13:32:01 +0530

    USe SQLite DB

--- src/libs/redis.js
+++ src/libs/redis.js
@@ -3,18 +3,21 @@ import redis from 'redis';
 /**
  * Create redis client instance
  */
-const redisClient = redis.createClient({
+const redisClient = process.env.REDIS_URI ? redis.createClient({
   url: process.env.REDIS_URI,
-});
+}) : { connected: false };
 
-redisClient.on('connect', () => {
-  // Successfully connected to redis!
-});
+if (process.env.REDIS_URI) {
+  redisClient.on('connect', () => {
+    // Successfully connected to redis!
+  });
 
-redisClient.on('error', (err) => {
-  // TODO: There is an error caused by redis. Send it to monitoring tool!!
-  // eslint-disable-next-line no-console
-  console.log('Redis error', err);
-});
+  redisClient.on('error', (err) => {
+    // TODO: There is an error caused by redis. Send it to monitoring tool!!
+    // eslint-disable-next-line no-console
+    console.log('Redis error', err);
+  });
+}
+
 
 export default redisClient;
